c4f1f66c61561129b48b6a7f33e42c37309f6c2d
rpdouglas <rpdouglas@gmail.com>
2025-10-20 15:34:02 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'recovery-agenda-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Placeholders for images/icons
+  './placeholder-icon-192.png',
+  './placeholder-icon-512.png',
+  // CDNs - Critical for the app to run
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com',
+];
+
+// Install event: cache all necessary assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache, adding URLs to cache.');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: serve cached content if available
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
